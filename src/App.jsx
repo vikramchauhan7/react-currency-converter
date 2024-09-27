0a2895f73191c18691733cce89d5e5f7284b1d8f
Vikram Chauhan <vikramchauhan@Vikrams-MacBook-Air.local>
2024-09-27 15:43:43 +0530

    update...

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="currency-converter">
+      <h2 className="converter-titile">Currency Converter</h2>
+      <form className="converter-form">
+        <div className="form-group">
+          <label className="form-label">Enter Amount</label>
+          <input type="number" className="form-input" required />
+        </div>
 
-export default App
+        <div className="form-group">
+          <div className="form-section">
+            <label className="form-label">Form</label>
+            <div className="currency-select">
+              <img src="https://flagsapi.com/US/flat/64.png" alt="Flag" />
+              <select className="currency-dropdown">
+                <option value="USD" selected>
+                  USD
+                </option>
+                <option value="INR">INR</option>
+                <option value="NPR">NPR</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="swap-icon">
+            <svg
+              width="16"
+              viewBox="0 0 20 19"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19.13 11.66H.22a.22.22 0 0 0-.22.22v1.62a.22.22 0 0 0 .22.22h16.45l-3.92 4.94a.22.22 0 0 0 .17.35h1.97c.13 0 .25-.06.33-.16l4.59-5.78a.9.9 0 0 0-.7-1.43zM19.78 5.29H3.34L7.26.35A.22.22 0 0 0 7.09 0H5.12a.22.22 0 0 0-.34.16L.19 5.94a.9.9 0 0 0 .68 1.4H19.78a.22.22 0 0 0 .22-.22V5.51a.22.22 0 0 0-.22-.22z"
+                fill="#000"
+              />
+            </svg>
+          </div>
+
+          <div className="form-section">
+            <label className="form-label">Form</label>
+            <div className="currency-select">
+              <img src="https://flagsapi.com/IN/flat/64.png" alt="Flag" />
+              <select className="currency-dropdown">
+                <option value="USD">USD</option>
+                <option value="INR" selected>
+                  INR
+                </option>
+                <option value="NPR">NPR</option>
+              </select>
+            </div>
+          </div>
+
+          <button type="submit" className="submit-button">
+            Get Exchange Rate
+          </button>
+
+          <p className="exchange-rate-result">1,000 USD = 83620.80 INR</p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default App;
